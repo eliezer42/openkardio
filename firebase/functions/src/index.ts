6dc9bcfb289a539018ee4d66e10c2181f0b9b716
eliezer42 <mendezdelgado42@gmail.com>
2023-08-17 07:23:56 -0600

    Html format added

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,36 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as nodemailer from "nodemailer";
 
 admin.initializeApp();
+
+function parseDateString(dateString: string): Date {
+  const parts = dateString.split("-");
+
+  const day = parseInt(parts[0]);
+  const month = parseInt(parts[1]) - 1; // Months in JavaScript are 0-indexed
+  const year = parseInt(parts[2]);
+
+  return new Date(year, month, day);
+}
+
+function calculateAge(birthdate: Date): number {
+  const currentDate = new Date();
+
+  const yearsDiff = currentDate.getFullYear() - birthdate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const birthMonth = birthdate.getMonth();
+
+  // Adjust age based on birth month
+  if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDate.getDate() < birthdate.getDate())) {
+    return yearsDiff - 1;
+  }
+
+  return yearsDiff;
+}
+
 
 exports.sendNotificationOnCreation = functions.database
   .ref("Cases/{caseId}")
@@ -14,6 +42,10 @@ exports.sendNotificationOnCreation = functions.database
     // Get the destination ID
     const destId = caseData.destination_id;
     const origId = caseData.origin_id;
+    const created = caseData.created;
+    const patientName = caseData.patient_first_name + " " + caseData.patient_last_name;
+    const patientRecord = caseData.patient_record;
+    const patientAge = calculateAge(parseDateString(caseData.patient_birth_date)).toString();
 
     // Get the email address
     const emailRef = admin.database().ref(`Hospitals/${destId}/email`);
@@ -29,11 +61,48 @@ exports.sendNotificationOnCreation = functions.database
       },
     });
 
+    const htmlContent = `
+<p><span style="font-family:Verdana,Geneva,sans-serif"><strong>DETALLES</strong></span></p>
+
+<table border="1" cellpadding="1" cellspacing="1" style="width:438.6px">
+<tbody>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>Fecha y Hora</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${created}</span></td>
+</tr>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>Origen</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${origId}</span></td>
+</tr>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>Nombre del paciente</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${patientName}</span></td>
+</tr>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>Edad</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${patientAge} años</span></td>
+</tr>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>Expediente</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${patientRecord}</span></td>
+</tr>
+<tr>
+<td style="width:129px"><span style="font-family:Verdana,Geneva,sans-serif"><strong>ID del Caso</strong></span></td>
+<td style="width:296px"><span style="font-family:Verdana,Geneva,sans-serif">${caseId}</span></td>
+</tr>
+</tbody>
+</table>
+
+<p>✅<span style="font-family:Verdana,Geneva,sans-serif">Ingrese a la aplicaci&oacute;n de <strong>OpenKardio</strong> para revisar el ex&aacute;men.</span></p>
+
+<p>&nbsp;</p>
+    `;
+
     const mailOptions = {
       from: "openkardio@gmail.com",
       to: email,
       subject: `Nuevo EKG para ${destId}`,
-      text: `Un nuevo caso fue enviado desde ${origId}`,
+      html: htmlContent,
     };
 
     await transporter.sendMail(mailOptions);
